d36af451b893f89c2a9582f45e1bf745f308a422
Iori KOTERA <kotera8300iori@gmail.com>
2023-07-20 12:49:31 +0900

    inputs

--- app/page.js
+++ app/page.js
@@ -13,6 +13,30 @@ export default function Home() {
 
       <div className={styles.grid}>
           <h2>
+            Input here
+          </h2>
+          <label for = 'start-date'> 開始日 
+            <input type="date" id = 'start-date'>
+          </label>
+          <label for = 'end-date'> 終了日 
+            <input type="date" id = 'end-date'>
+          </label>
+
+          <label> 平日の候補時間帯 
+            <div class="weekday">
+              <input type= 'time' v-model = 'WD["from"]'>
+              <input type= 'time' v-model = 'WD["to"]'>
+            </div>
+          </label>
+          
+          <div>
+            <button class = 'addButton' > － </button>
+            <button class = 'addButton'> ＋ </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.grid}>
+          <h2>
             Deploy <span>-&gt;</span>
           </h2>
           <p>
